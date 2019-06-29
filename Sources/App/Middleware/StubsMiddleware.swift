e82581024efb6b0e41d1449e986703c64bd133ba
Cameron <cameronporter95@gmail.com>
2019-06-29 16:50:39 +1200

    Removed the need to set a directory environment variable

--- Sources/App/Middleware/StubsMiddleware.swift
+++ Sources/App/Middleware/StubsMiddleware.swift
@@ -10,40 +10,42 @@ import Vapor
 import Foundation
 
 struct RouteError: Error {
-    let message: String
+  let message: String
 }
 
-private let projectDir = ProcessInfo.processInfo.environment["PROJECT_DIR"]
-
 final class StubsMiddleware: Middleware {
-    public func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
-        guard let projectDir = projectDir else {
-            throw RouteError(message: "PROJECT_DIR environment variable not set")
-        }
-        
-        let method = request.http.method
-        let path = request.http.url.path.dropFirst().replacingOccurrences(of: "/", with: ".")
-        let query = request.http.url.query
-        let queryfilePath = "\(projectDir)/Stubs/\(path)?\(query ?? "").\(method).json"
-        let straightfilePath = "\(projectDir)/Stubs/\(path).\(method).json"
-        
-        var isDir: ObjCBool = false
-        if FileManager.default.fileExists(atPath: queryfilePath, isDirectory: &isDir) {
-            print(queryfilePath)
-            return try request.streamFile(at: queryfilePath)
-        } else {
-            if FileManager.default.fileExists(atPath: straightfilePath, isDirectory: &isDir) {
-                print(straightfilePath)
-                return try request.streamFile(at: straightfilePath)
-            } else {
-                return try next.respond(to: request)
-            }
-        }
+  public func respond(to request: Request, chainingTo next: Responder) throws -> Future<Response> {
+    let projectDir = DirectoryConfig.detect().workDir
+    let method = request.http.method
+    let path = request.http.url.path.dropFirst().replacingOccurrences(of: "/", with: ".")
+    let query = request.http.url.query
+    let straightfilePath = URL(fileURLWithPath: projectDir)
+      .appendingPathComponent("Stubs", isDirectory: true)
+      .appendingPathComponent("\(path).\(method).json", isDirectory: false).path
+    
+    let queryfilePath = URL(fileURLWithPath: projectDir)
+      .appendingPathComponent("Stubs", isDirectory: true)
+      .appendingPathComponent("\(path)?\(query ?? "").\(method).json", isDirectory: false).path
+    
+    var isDir: ObjCBool = false
+    if FileManager.default.fileExists(atPath: queryfilePath, isDirectory: &isDir) {
+      print(queryfilePath)
+      return try request.streamFile(at: queryfilePath)
+    } else {
+      if FileManager.default.fileExists(atPath: straightfilePath, isDirectory: &isDir) {
+        print(straightfilePath)
+        return try request.streamFile(at: straightfilePath)
+      } else {
+        print("couldn't find any files")
+        return try next.respond(to: request)
+      }
     }
+  }
 }
 
 extension StubsMiddleware: ServiceType {
-    static func makeService(for worker: Container) throws -> Self {
-        return .init()
-    }
+  static func makeService(for worker: Container) throws -> Self {
+    return .init()
+  }
 }
+
